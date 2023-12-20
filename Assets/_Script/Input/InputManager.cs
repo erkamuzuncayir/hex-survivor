9d1f2d3342b4a4d5e2f2fb152346e52397e57a0c
erkamUzuncayir <erkamuzuncayir@gmail.com>
2023-12-20 09:08:44 +0300

    PlayerInputHandler.cs: Rename class to InputManager.cs and add ClickInputHandler implementation
    Added in OnMouseLeftClickPerformed method

---
+++ Assets/_Script/Input/InputManager.cs
@@ -0,0 +1,72 @@
+using System;
+using _Script.PersonalAPI.Data.RuntimeSet;
+using _Script.PersonalAPI.Event;
+using _Script.PersonalAPI.Input;
+using _Script.System;
+using _Script.System.StateSystem.State;
+using Cinemachine;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace _Script.Input
+{
+    public class InputManager : MonoBehaviour
+    {
+        // Camera
+        [SerializeField] private GameObjectRuntimeSet so_rs_playerCam;
+        private Camera _playerCam;
+        
+        // Unity's InputSystem related
+        public InputActionReference MouseLeftClickReference;
+        public InputActionReference MousePositionReference;
+        private InputAction _ia_mouseLeftClick;
+        private InputAction _ia_mousePosition;
+    
+        // Cache fields
+        private GameObject _targetedGameObject;
+        
+        // Fills InputAction fields.
+        private void Awake()
+        {
+            _ia_mouseLeftClick = MouseLeftClickReference.action;
+            _ia_mousePosition = MousePositionReference.action;
+        }
+
+        // Subscribes InputAction methods
+        private void OnEnable()
+        {
+            _ia_mousePosition.Enable();
+            _ia_mouseLeftClick.Enable();
+            _ia_mouseLeftClick.performed += OnMouseLeftClickPerformed;
+            _ia_mouseLeftClick.canceled += OnMouseLeftClickCanceled;
+        }
+
+        private void Start()
+        {
+            _playerCam = so_rs_playerCam.Items[0].GetComponent<Camera>();
+        }
+
+        private void OnDisable()
+        {
+            _ia_mousePosition.Disable();
+            _ia_mouseLeftClick.Disable();
+        }
+
+        // Pass inputs to related systems according to game state system.
+        private void OnMouseLeftClickPerformed(InputAction.CallbackContext obj)
+        { 
+            Vector2 mouseScreenPos = _ia_mousePosition.ReadValue<Vector2>();
+            Vector2 mouseWorldPos = _playerCam.ScreenToWorldPoint(mouseScreenPos);
+            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
+            
+            if (hit.collider.gameObject == null) return;
+            if(hit.collider.gameObject.GetComponent<ClickInputHandler>() == null) return;
+
+            hit.collider.gameObject.GetComponent<ClickInputHandler>().OnClickPerformed();
+        }
+        
+        private void OnMouseLeftClickCanceled(InputAction.CallbackContext obj)
+        {
+        }
+    }
+}
