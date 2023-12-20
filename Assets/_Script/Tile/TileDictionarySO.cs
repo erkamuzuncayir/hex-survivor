be72902f32bbc2169d07df9683cf13931764d2ec
erkamUzuncayir <erkamuzuncayir@gmail.com>
2023-12-21 00:44:09 +0300

    TileDictionarySO.cs: Remove old implementation add new one

---
+++ Assets/_Script/Tile/TileDictionarySO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace _Script.Tile
+{
+    [CreateAssetMenu(fileName = "so_TileDictionary", menuName = "Data/Tile Dictionary")]
+    public class TileDictionarySO : ScriptableObject
+    {
+        public List<TileKeyValuePair> BaseTiles = new List<TileKeyValuePair>();
+    }
+
+    [Serializable]
+    public struct TileKeyValuePair
+    {
+        public Vector3Int Coord;
+        public TileData TileData;
+    }
+}
