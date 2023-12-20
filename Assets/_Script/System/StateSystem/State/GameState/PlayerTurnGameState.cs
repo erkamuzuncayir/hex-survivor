39f5da3a4175389c50f05a1552f579e14a470b0d
erkamUzuncayir <erkamuzuncayir@gmail.com>
2023-12-20 23:17:27 +0300

    PlayerTurnGameState.cs: Create game state that contain general game logic while player turn

---
+++ Assets/_Script/System/StateSystem/State/GameState/PlayerTurnGameState.cs
@@ -0,0 +1,26 @@
+using _Script.PersonalAPI.StateMachine;
+using _Script.System.StateSystem.StateMachine;
+using UnityEngine;
+
+namespace _Script.System.StateSystem.State.GameState
+{
+    [CreateAssetMenu(fileName = "PlayerTurnGameState", menuName = "System/State/Game State/Player Turn")]
+    public class PlayerTurnGameState : GameStateSO
+    {
+        public override void InitState(IStateMachine<GameStateMachine, GameStateSO> stateMachine)
+        {
+        }
+
+        public override void EnterState()
+        {
+        }
+
+        public override void UpdateState()
+        {
+        }
+
+        public override void ExitState()
+        {
+        }
+    }
+}
