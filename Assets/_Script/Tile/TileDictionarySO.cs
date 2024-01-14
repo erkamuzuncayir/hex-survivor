34860d7f335683b352ce98de7c173492b44003b2
erkamUzuncayir <erkamuzuncayir@gmail.com>
2024-01-14 18:34:39 +0300

    Add overload for GetTileData method and add new field for TileKeyValuePair

--- Assets/_Script/Tile/TileDictionarySO.cs
+++ Assets/_Script/Tile/TileDictionarySO.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace _Script.Tile
@@ -7,17 +6,44 @@ namespace _Script.Tile
     [CreateAssetMenu(fileName = "so_TileDictionary", menuName = "Data/Tile Dictionary")]
     public class TileDictionarySO : ScriptableObject
     {
-        public List<TileKeyValuePair> GroundTiles = new List<TileKeyValuePair>();
-    }
+        public TileKeyValuePair[] GroundTiles;
 
+        public void InitDictionary(int tileCount)
+        {
+            GroundTiles = new TileKeyValuePair[tileCount];
+        }
+
+        public GroundTileData GetTileData(int index, Vector3Int coord)
+        {
+            if (GroundTiles[index].Coord == coord)
+                return GroundTiles[index].GroundTileData;
+            
+            Debug.LogError($"there is no tile on {coord}!");
+            return new EmptyGroundTile(-1, Vector3.zero, Vector3Int.zero, TileType.Empty, false);
+        }
+
+        
+        public GroundTileData GetTileData(Vector3Int coord)
+        {
+            for (int i = 0; i < GroundTiles.Length; i++)
+                if (GroundTiles[i].Coord == coord)
+                    return GroundTiles[i].GroundTileData;
+            
+            Debug.LogError($"there is no tile on {coord}!");
+            return new EmptyGroundTile(-1, Vector3.zero, Vector3Int.zero, TileType.Empty, false);
+        }
+    }
+    
     [Serializable]
     public struct TileKeyValuePair
     {
+        public int DictIndex;
         public Vector3Int Coord;
         public GroundTileData GroundTileData;
 
-        public TileKeyValuePair(Vector3Int coord, GroundTileData groundTileData)
+        public TileKeyValuePair(int dictIndex, Vector3Int coord, GroundTileData groundTileData)
         {
+            DictIndex = dictIndex;
             Coord = coord;
             GroundTileData = groundTileData;
         }
