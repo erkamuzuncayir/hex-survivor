62275a5b0843f491fc729ea02e6ac8d827e94116
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-01-29 22:48:45 +0300

    Create PlayerStateSOReference class for holding PlayerStateSO value

---
+++ Assets/_Script/Data/PlayerStateSOReference.cs
@@ -0,0 +1,11 @@
+using _Script.PersonalAPI.Data.Type;
+using _Script.System.StateSystem.State.PlayerState;
+using UnityEngine;
+
+namespace _Script.Data
+{
+    [CreateAssetMenu(fileName = "so_ref_PlayerStateSO", menuName = "Data/Reference/Player State SO")]
+    public class PlayerStateSOReference : DataSO<PlayerStateSO>
+    {
+    }
+}
