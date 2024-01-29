31745bd8411c98219f13b7b72801d43ef36c0072
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-01-29 22:50:05 +0300

    Implement hover checking and passing game objects that contain HoverInputHandler as a component in InputManager.cs

--- Assets/_Script/Input/InputManager.cs
+++ Assets/_Script/Input/InputManager.cs
@@ -2,6 +2,7 @@ using _Script.PersonalAPI.Data.RuntimeSet;
 using _Script.PersonalAPI.Input;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 namespace _Script.Input
 {
@@ -10,16 +11,21 @@ namespace _Script.Input
         // Camera
         [SerializeField] private GameObjectRuntimeSet _so_rs_playerCam;
         private Camera _playerCam;
-        
+
         // Unity's InputSystem related
         public InputActionReference MouseLeftClickReference;
         public InputActionReference MousePositionReference;
         private InputAction _ia_mouseLeftClick;
         private InputAction _ia_mousePosition;
-    
+
+        // Layers
+        [SerializeField] private LayerMask _tilemapLayer;
+
         // Cache fields
-        private GameObject _targetedGameObject;
-        
+        [SerializeField] private GameObjectRuntimeSet _so_rs_pfb_tilemap_base;
+        private Tilemap _baseTilemap;
+        private HoverInputHandler _lastHoveredGOHoverInputHandler;
+
         // Fills InputAction fields.
         private void Awake()
         {
@@ -33,30 +39,64 @@ namespace _Script.Input
             _ia_mousePosition.Enable();
             _ia_mouseLeftClick.Enable();
             _ia_mouseLeftClick.performed += OnMouseLeftClickPerformed;
-            _ia_mouseLeftClick.canceled += OnMouseLeftClickCanceled;
+            _ia_mousePosition.performed += OnMouseMoved;
         }
 
         private void Start()
         {
             _playerCam = _so_rs_playerCam.Items[0].GetComponent<Camera>();
+            _baseTilemap = _so_rs_pfb_tilemap_base.Items[0].GetComponent<Tilemap>();
         }
 
         private void OnDisable()
         {
+            _ia_mouseLeftClick.performed -= OnMouseLeftClickPerformed;
+            _ia_mousePosition.performed -= OnMouseMoved;
             _ia_mousePosition.Disable();
             _ia_mouseLeftClick.Disable();
         }
 
+        private void OnMouseMoved(InputAction.CallbackContext obj)
+        {
+            ProcessHover();
+        }
+
+        private void ProcessHover()
+        {
+            Vector2 inputWorldPos = GetInputWorldPosition();
+            RaycastHit2D hit = Physics2D.Raycast(inputWorldPos, Vector2.zero);
+
+            if (hit.collider == null) return;
+            if (!hit.collider.gameObject.TryGetComponent(out HoverInputHandler hoverInputHandler)) return;
+
+            Debug.Log(inputWorldPos);
+            if (_lastHoveredGOHoverInputHandler != null)
+                _lastHoveredGOHoverInputHandler.OnHoverCanceled();
+
+            _lastHoveredGOHoverInputHandler = hoverInputHandler;
+            _lastHoveredGOHoverInputHandler.OnHoverPerformed.Invoke(inputWorldPos);
+        }
+
         // Pass inputs to related systems according to game state system.
         private void OnMouseLeftClickPerformed(InputAction.CallbackContext inputData)
-        { 
-            Vector2 mouseScreenPos = _ia_mousePosition.ReadValue<Vector2>();
-            Vector2 mouseWorldPos = _playerCam.ScreenToWorldPoint(mouseScreenPos);
-            RaycastHit2D hit = Physics2D.Raycast(mouseWorldPos, Vector2.zero);
-            
-            if (hit.collider.gameObject == null) return;
-            if(hit.collider.gameObject.GetComponent<ClickInputHandler>() == null) return;
-            hit.collider.gameObject.GetComponent<ClickInputHandler>().OnClickPerformed.Invoke(mouseWorldPos);
+        {
+            Vector2 inputWorldPos = GetInputWorldPosition();
+            RaycastHit2D hit = Physics2D.Raycast(inputWorldPos, Vector2.zero);
+
+            if (hit.collider == null) return;
+            if (!hit.collider.gameObject.TryGetComponent(out ClickInputHandler clickInputHandler))
+                return;
+            if (hit.collider.gameObject.TryGetComponent(out HoverInputHandler hoverInputHandler))
+                hoverInputHandler.OnHoverCanceled();
+
+            clickInputHandler.OnClickPerformed.Invoke(inputWorldPos);
+        }
+
+        private Vector2 GetInputWorldPosition()
+        {
+            Vector2 inputScreenPos = _ia_mousePosition.ReadValue<Vector2>();
+            Vector2 inputWorldPos = _playerCam.ScreenToWorldPoint(inputScreenPos);
+            return inputWorldPos;
         }
     }
 }
