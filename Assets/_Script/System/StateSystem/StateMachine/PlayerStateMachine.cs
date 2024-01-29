679b4ed92c63a5225a1cb3ab4c3e8746a0f9d468
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-01-29 22:53:55 +0300

    Minor refactoring in PlayerStateMachine.cs and add current state to a PlayerStateSOReference asset

--- Assets/_Script/System/StateSystem/StateMachine/PlayerStateMachine.cs
+++ Assets/_Script/System/StateSystem/StateMachine/PlayerStateMachine.cs
@@ -1,17 +1,19 @@
 using System.Collections.Generic;
 using _Script.Actors;
+using _Script.Data;
 using _Script.PersonalAPI.Input;
 using _Script.PersonalAPI.StateMachine;
 using _Script.System.StateSystem.State.PlayerState;
 using UnityEngine;
 
 namespace _Script.System.StateSystem.StateMachine
-{ 
+{
     public class PlayerStateMachine : MonoBehaviour, IStateMachine<PlayerStateMachine, PlayerStateSO>
     {
         // State List and States
         public List<IState<PlayerStateMachine, PlayerStateSO>> AllPlayerStates = new();
-        public IState<PlayerStateMachine, PlayerStateSO> CurrentPlayerState;
+        public PlayerStateSOReference so_ref_state_Player_Current;
+        private PlayerStateSO so_state_player_current;
         public PlayerStateSO so_state_PlayerIdle_Unselected;
         public PlayerStateSO so_state_PlayerIdle_Selected;
         public PlayerStateSO so_state_PlayerOffTurn;
@@ -20,17 +22,17 @@ namespace _Script.System.StateSystem.StateMachine
 
         // Input Handler
         [SerializeField] private ClickInputHandler _clickInputHandler;
-        
+
         // Cache fields
         [SerializeField] private GameObject _go_player;
         [SerializeField] private PlayerDataSO _playerDataSO;
-        
+
         private void Awake()
         {
             FillListWithStates();
             _playerDataSO.PlayerCoord = Vector3Int.zero;
         }
-        
+
         private void FillListWithStates()
         {
             AllPlayerStates.Add(so_state_PlayerIdle_Unselected);
@@ -39,7 +41,7 @@ namespace _Script.System.StateSystem.StateMachine
             AllPlayerStates.Add(so_state_PlayerMove);
             AllPlayerStates.Add(so_state_PlayerAttack);
         }
-        
+
         public void InitStates()
         {
             foreach (IState<PlayerStateMachine, PlayerStateSO> state in AllPlayerStates)
@@ -61,10 +63,10 @@ namespace _Script.System.StateSystem.StateMachine
         {
             _clickInputHandler.OnClickPerformed -= OnMouseClickPerformed;
         }
-        
+
         public void OnMouseClickPerformed(Vector2 inputWorldPos)
         {
-            switch (CurrentPlayerState)
+            switch (so_state_player_current)
             {
                 case IdleSelectedPlayerStateSO:
                 {
@@ -78,19 +80,20 @@ namespace _Script.System.StateSystem.StateMachine
                 }
             }
         }
-        
+
         public void HandleState(PlayerStateSO requestedState)
         {
-            if (CurrentPlayerState == requestedState)
+            if (so_state_player_current == requestedState)
                 return;
-            CurrentPlayerState?.ExitState();
-            CurrentPlayerState = requestedState;
-            CurrentPlayerState.EnterState();
+            so_state_player_current?.ExitState();
+            so_state_player_current = requestedState;
+            so_ref_state_Player_Current.Value = requestedState;
+            so_state_player_current.EnterState();
         }
 
         public void ProcessState(PlayerStateSO requestedState)
         {
-            CurrentPlayerState.UpdateState();
+            so_state_player_current.UpdateState();
         }
     }
 }
