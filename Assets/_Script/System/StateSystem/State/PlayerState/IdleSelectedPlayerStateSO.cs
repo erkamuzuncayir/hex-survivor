329ddf8a2910f0479fcc4546728f5eb09c9a3dc8
erkamUzuncayir <erkamuzuncayir@gmail.com>
2024-02-02 09:48:09 +0300

    Create a player selected underlay mechanic for when player selected and assign references for this logic

--- Assets/_Script/System/StateSystem/State/PlayerState/IdleSelectedPlayerStateSO.cs
+++ Assets/_Script/System/StateSystem/State/PlayerState/IdleSelectedPlayerStateSO.cs
@@ -1,5 +1,6 @@
 using _Script.Actors;
 using _Script.Event;
+using _Script.PersonalAPI.Data.RuntimeSet;
 using _Script.PersonalAPI.StateMachine;
 using _Script.System.StateSystem.StateMachine;
 using UnityEngine;
@@ -14,19 +15,33 @@ namespace _Script.System.StateSystem.State.PlayerState
 
         [Header("Events")] [SerializeField] private IntEventSO _so_event_SelectedTileDictIndex;
 
-        private UnityEvent<int> _event_SelectedTileDictIndex;
-        [SerializeField] private PlayerDataSO _playerDataSO;
+        [SerializeField] private GameObject pfb_gfx_player_selected_underlay;
+        private GameObject gfx_player_selected_underlay;
+        private static readonly Vector3 _SELECTED_UNDERLAY_OFFSET = new Vector3(0, -0.05f, 0);
+        
+        private UnityEvent<int> _event_SelectedTileDictIndex;        
+        [SerializeField] private GameObjectRuntimeSet _so_rs_player;
+        [SerializeField] private PlayerDataSO _so_playerData;
+        
+        // Cache Fields
+        private Transform _playerTransform;
+
 
         public override void InitState(IStateMachine<PlayerStateMachine, PlayerStateSO> stateMachine)
         {
             _playerStateMachine = (PlayerStateMachine)stateMachine;
+            _playerTransform = _so_rs_player.Items[0].transform;
             _event_SelectedTileDictIndex = new UnityEvent<int>();
+            gfx_player_selected_underlay = Instantiate(pfb_gfx_player_selected_underlay, _playerTransform, true);
+            gfx_player_selected_underlay.SetActive(false);
         }
 
         public override void EnterState()
         {
             _so_event_SelectedTileDictIndex.RegisterListenerDirectly(_event_SelectedTileDictIndex);
             _event_SelectedTileDictIndex.AddListener(OnTargetTileSelected);
+            gfx_player_selected_underlay.transform.position = _SELECTED_UNDERLAY_OFFSET + _playerTransform.position;
+            gfx_player_selected_underlay.SetActive(true);
         }
 
 
@@ -36,7 +51,7 @@ namespace _Script.System.StateSystem.State.PlayerState
 
         private void OnTargetTileSelected(int selectedTileDictIndex)
         {
-            _playerDataSO.TargetTileDictIndex = selectedTileDictIndex;
+            _so_playerData.TargetTileDictIndex = selectedTileDictIndex;
             _playerStateMachine.HandleState(_playerStateMachine.so_state_PlayerMove);
         }
 
@@ -44,6 +59,7 @@ namespace _Script.System.StateSystem.State.PlayerState
         {
             _event_SelectedTileDictIndex.RemoveListener(OnTargetTileSelected);
             _so_event_SelectedTileDictIndex.UnregisterListenerDirectly(_event_SelectedTileDictIndex);
+            gfx_player_selected_underlay.SetActive(false);
         }
     }
 }
