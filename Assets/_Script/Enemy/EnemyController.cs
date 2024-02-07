ed02c6c26ba221ef12a9a80311e61275afc7de86
erkamUzuncayir <erkamuzuncayir@gmail.com>
2024-02-07 08:17:20 +0300

    Start to create EnemyController.cs script for manage basic control of enemy

---
+++ Assets/_Script/Enemy/EnemyController.cs
@@ -0,0 +1,53 @@
+using _Script.Tile;
+using UnityEngine;
+
+namespace _Script.Enemy
+{
+    public class EnemyController : MonoBehaviour
+    {
+        public EnemyTypeSO so_Type;
+        public EnemyType EType;
+        
+        [Header("Navigation Data")]
+        public Vector3Int Coord; // It'll be filled first from EnemySpawner.cs
+        public int CurTileDictIndex;
+        public int PlayerTileDictIndex;
+        public GroundTileData TileUnderTheEnemy;
+        
+        [Header("Gameplay Data")]
+        public int RemainingMoveCount;
+        public int AttackRange;
+        public int Damage;
+        public int Health;
+        
+        [Header("Default Values")]
+        public int MoveRegenEndOfTurn;
+        public int HealthRegenEndOfTurn;
+        public int MaxMoveCount { get; private set; }
+        public int MaxAttackRange { get; private set; }
+        public int MaxDamage { get; private set; }
+        public int MaxHealth { get; private set; }
+        
+        [Header("Movement Data")] 
+        public float StepCountBetweenTwoTile;
+
+        public void Initialize()
+        {
+            GetDataFromEnemyType();
+        }
+
+        private void GetDataFromEnemyType()
+        {
+            MoveRegenEndOfTurn = so_Type.MoveRegenEndOfTurn;
+            HealthRegenEndOfTurn = so_Type.HealthRegenEndOfTurn;
+            MaxMoveCount = so_Type.MaxMoveCount;
+            MaxAttackRange = so_Type.MaxAttackRange;
+            MaxDamage = so_Type.MaxDamage;
+            MaxHealth = so_Type.MaxHealth;
+            AttackRange = so_Type.AttackRange;
+            Damage = so_Type.Damage;
+            Health = so_Type.Health;
+            RemainingMoveCount = MaxMoveCount;
+        }
+    }
+}
