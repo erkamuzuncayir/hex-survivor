f7a83246769e0831de4bf1f0db2a5bd6bb1a4908
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-02-19 01:57:33 +0300

    Modified PlayerTurnGameStateSO.cs:
        Added a field _so_stateMachine_game to cache the GameStateMachine.
        Added fields for runtime sets and events related to player states.
        Initialized _so_stateMachine_game in InitState.
        Registered and unregistered listeners for player state events in EnterState and ExitState.
        Added OnPlayerStateChanged method to handle state transition to EnemyTurnGameStateSO when the player's state changes to OffTurnPlayerStateSO.

--- Assets/_Script/System/StateSystem/State/GameState/PlayerTurnGameStateSO.cs
+++ Assets/_Script/System/StateSystem/State/GameState/PlayerTurnGameStateSO.cs
@@ -1,9 +1,12 @@
 using _Script.Actors;
 using _Script.Data;
+using _Script.Event;
 using _Script.PersonalAPI.Data.RuntimeSet;
 using _Script.PersonalAPI.StateMachine;
+using _Script.System.StateSystem.State.PlayerState;
 using _Script.System.StateSystem.StateMachine;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.Serialization;
 
 namespace _Script.System.StateSystem.State.GameState
@@ -11,14 +14,25 @@ namespace _Script.System.StateSystem.State.GameState
     [CreateAssetMenu(fileName = "so_state_game_PlayerTurn", menuName = "System/State/Game State/Player Turn")]
     public class PlayerTurnGameStateSO : GameStateSO
     {
-        // Cache Fields
-        [SerializeField] private PlayerStateMachineReference _so_ref_stateMachine_Player;
-        [SerializeField] private PlayerDataSO _so_playerData;
+        [Header("Systems")] 
+        private GameStateMachine _so_stateMachine_game;
+
+        [Header("Runtime Sets")]
         [SerializeField] private RuntimeSet<GameObject> _so_rs_go_player;
+        
+        [Header("Events")]
+        [SerializeField] private PlayerStateSOEventSO _so_event_playerStateSO_changed;
+        private UnityEvent<PlayerStateSO> _bridge_event_playerStateSO_changed;
+        
+        [Header("Cache fields")]
+        [SerializeField] private PlayerStateMachineReference _so_ref_stateMachine_Player;
+        [SerializeField] private OffTurnPlayerStateSO _so_state_PlayerOffTurn;
+        [SerializeField] private PlayerDataSO _so_playerData;
         private GameObject _go_player;
         
         public override void InitState(IStateMachine<GameStateMachine, GameStateSO> stateMachine)
         {
+            _so_stateMachine_game = (GameStateMachine)stateMachine;
             _go_player = _so_rs_go_player.Items[0];
             SetPlayerInitialCoordToPlayerData();
         }
@@ -31,6 +45,14 @@ namespace _Script.System.StateSystem.State.GameState
 
         public override void EnterState()
         {
+            _so_event_playerStateSO_changed.RegisterListenerDirectly(_bridge_event_playerStateSO_changed);
+            _bridge_event_playerStateSO_changed.AddListener(OnPlayerStateChanged);
+        }
+
+        private void OnPlayerStateChanged(PlayerStateSO currentPlayerStateSO)
+        {
+            if(currentPlayerStateSO == _so_state_PlayerOffTurn)
+                _so_stateMachine_game.HandleState(_so_stateMachine_game.so_state_game_EnemyTurn);
         }
 
         public override void UpdateState()
@@ -39,6 +61,8 @@ namespace _Script.System.StateSystem.State.GameState
 
         public override void ExitState()
         {
+            _bridge_event_playerStateSO_changed.RemoveListener(OnPlayerStateChanged);
+            _so_event_playerStateSO_changed.UnregisterListenerDirectly(_bridge_event_playerStateSO_changed);
         }
     }
 }
