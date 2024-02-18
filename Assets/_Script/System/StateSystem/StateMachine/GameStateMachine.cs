12e32ccf5d617e027298755c30ebb2e42c93ee5a
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-02-19 00:20:37 +0300

    Refactor GameStateMachine to use MonoBehaviour and handle states more efficiently
    - Converted GameStateMachine to inherit MonoBehaviour.
    - Initialized state list in Awake method.
    - Moved state initialization logic to Start method.
    - Added a method to fill the state list with available states.
    - Refactored HandleState method to properly handle state transitions and event raising.
    - Updated ProcessState method to call the UpdateState method of the current state.

--- Assets/_Script/System/StateSystem/StateMachine/GameStateMachine.cs
+++ Assets/_Script/System/StateSystem/StateMachine/GameStateMachine.cs
@@ -1,57 +1,63 @@
+using UnityEngine;
 using System.Collections.Generic;
+using _Script.Data;
+using _Script.PersonalAPI.Event;
 using _Script.PersonalAPI.StateMachine;
 using _Script.System.StateSystem.State.GameState;
 
 namespace _Script.System.StateSystem.StateMachine
 {
-    public class GameStateMachine : IStateMachine<GameStateMachine, GameStateSO>
+    public class GameStateMachine : MonoBehaviour, IStateMachine<GameStateMachine, GameStateSO>
     {
-        public List<GameStateSO> CurrentGameStates { get; private set; }
+        // State List and States
+        private List<GameStateSO> list_state_game_all;
+        public GameStateSOReference so_ref_state_Game_Current;
+        private GameStateSO so_state_game_current;
+        public GameStateSO so_state_game_MainMenu;
+        public GameStateSO so_state_game_PlayerTurn;
+        public GameStateSO so_state_game_EnemyTurn;
         
+        // Event
+        [SerializeField] private ParamEventSO<GameStateSO> _so_event_state_game_current;
+        
+        private void Awake()
+        {
+            FillListWithStates();
+        }
+
+        public void FillListWithStates()
+        {
+            list_state_game_all.Add(so_state_game_MainMenu);
+            list_state_game_all.Add(so_state_game_PlayerTurn);
+            list_state_game_all.Add(so_state_game_EnemyTurn);
+        }
+
+        private void Start()
+        {
+            InitStates();
+            HandleState(so_state_game_PlayerTurn);
+        }
+
         public void InitStates()
         {
+            foreach (GameStateSO gameStateSO in list_state_game_all)
+                gameStateSO.InitState(this);
         }
 
         public void HandleState(GameStateSO requestedState)
         {
-        }
-
-        private void AddStateToList(GameStateSO stateToBeAdd)
-        {
-            CurrentGameStates.Add(stateToBeAdd);
-            StartState(stateToBeAdd);
-        }
-
-        private void ProcessStates()
-        {
-            for (int i = 0; i < CurrentGameStates.Count; i++)
-            { 
-                CurrentGameStates[i].UpdateState();
-            }
-        }
-
-        private void RemoveStateFromList(GameStateSO stateToBeRemove)
-        {
-            stateToBeRemove.ExitState();
-            CurrentGameStates.Remove(stateToBeRemove);
-        }
-        
-        private void RemoveAllStatesFromList()
-        {
-            for (int i = 0; i < CurrentGameStates.Count; i++)
-            { 
-                CurrentGameStates[i].ExitState();
-            }
-            CurrentGameStates.Clear();
-        }
-        
-        private void StartState(GameStateSO stateToBeStart)
-        {
-            stateToBeStart.EnterState();
+            if (so_state_game_current == requestedState)
+                return;
+            so_state_game_current?.ExitState();
+            so_state_game_current = requestedState;
+            so_ref_state_Game_Current.Value = requestedState;
+            so_state_game_current.EnterState();
+            _so_event_state_game_current.Raise(so_state_game_current);
         }
         
         public void ProcessState(GameStateSO requestedState)
         {
+            so_state_game_current.UpdateState();
         }
     }
 }
