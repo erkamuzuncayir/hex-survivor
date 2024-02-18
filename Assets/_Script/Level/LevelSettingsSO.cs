596649c55c0e14354a84d079f9fefa31040e5a33
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-02-19 00:15:39 +0300

    Add LevelSettings ScriptableObject
    Added LevelSettingsSO scriptable object to manage level settings and spawnable enemies.

--- Assets/_Script/Level/LevelSettingsSO.cs
+++ Assets/_Script/Level/LevelSettingsSO.cs
@@ -1,8 +1,10 @@
+using System;
 using _Script.Enemy;
 using UnityEngine;
 
 namespace _Script.Level
 {
+    [CreateAssetMenu(fileName = "so_LevelSettings_*", menuName = "LevelSettings")]
     public class LevelSettingsSO : ScriptableObject
     {
         [Header("General Settings")] 
@@ -12,6 +14,7 @@ namespace _Script.Level
         public SpawnableEnemy[] SpawnableEnemies;
     }
 
+    [Serializable]
     public class SpawnableEnemy
     {
         public EnemyTypeSO so_Enemy;
