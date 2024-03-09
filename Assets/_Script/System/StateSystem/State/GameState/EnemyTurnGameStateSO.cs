15624a1fb4a8db8d9c667fa7fb82d3708d22e32a
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-03-09 20:18:30 +0300

    Add EnemyManager dependency and call OnEnemyTurn method in EnterState
    Added a dependency on EnemyManager in EnemyTurnGameStateSO.cs to handle enemy turn logic. In the EnterState method, called the OnEnemyTurn method asynchronously and transitioned to the PlayerTurn state afterward.

--- Assets/_Script/System/StateSystem/State/GameState/EnemyTurnGameStateSO.cs
+++ Assets/_Script/System/StateSystem/State/GameState/EnemyTurnGameStateSO.cs
@@ -1,3 +1,4 @@
+using _Script.Enemy;
 using _Script.PersonalAPI.StateMachine;
 using _Script.System.StateSystem.StateMachine;
 using UnityEngine;
@@ -7,13 +8,19 @@ namespace _Script.System.StateSystem.State.GameState
 {
     [CreateAssetMenu(fileName = "so_state_game_EnemyTurn", menuName = "System/State/Game State/Enemy Turn")]
     public class EnemyTurnGameStateSO : GameStateSO
-    {
+    { 
+        private GameStateMachine _so_stateMachine_game;
+        [SerializeField] private EnemyManager _so_enemyManager;
+        
         public override void InitState(IStateMachine<GameStateMachine, GameStateSO> stateMachine)
-        {
+        { 
+            _so_stateMachine_game = (GameStateMachine)stateMachine;
         }
 
-        public override void EnterState()
+        public override async void EnterState()
         {
+            await _so_enemyManager.OnEnemyTurn();
+            _so_stateMachine_game.HandleState(_so_stateMachine_game.so_state_game_PlayerTurn);
         }
 
         public override void UpdateState()
