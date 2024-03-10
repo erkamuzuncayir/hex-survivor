26925a420b57655bc9f639c4598016011d04e0a4
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-03-10 16:05:13 +0300

    Enhance MapGenerator.cs for customizable map generation
        Added parameters for controlling octaves, persistance, lacunarity, seed, and offset, providing more control over the generated noise map.
        Implemented validation in OnValidate() to ensure parameters are within valid ranges.
        Improved readability and maintainability of the code.
    
    This commit extends the functionality of the MapGenerator, allowing users to fine-tune the generation process for their specific needs

--- Assets/_Script/MapGeneration/MapGenerator.cs
+++ Assets/_Script/MapGeneration/MapGenerator.cs
@@ -1,3 +1,4 @@
+using System;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -9,18 +10,39 @@ namespace _Script.MapGeneration
         public int MapWidth;
         public int MapHeight;
         public float NoiseScale;
+
+        public int octaves;
+        [Range(0,1)]
+        public float persistance;
+        public float lacunarity;
+
+        public int seed;
+        public Vector2 offset;
+        
         public bool AutoUpdate;
+        
         
         [Button()]
         public void GenerateMap()
         {
-            float[,] noiseMap = Noise.GenerateNoiseMap(MapWidth, MapHeight, NoiseScale);
+            float[,] noiseMap = Noise.GenerateNoiseMap(MapWidth, MapHeight, seed, NoiseScale, octaves, persistance, lacunarity, offset);
 
             _display.DrawNoiseMap(noiseMap);
-            foreach (var VARIABLE in noiseMap)
-            {
-                Debug.Log(VARIABLE);
-            }
+        }
+
+        private void OnValidate()
+        {
+            if (MapWidth < 1)
+                MapWidth = 1;
+            
+            if (MapHeight < 1)
+                MapHeight = 1;
+
+            if (lacunarity < 1)
+                lacunarity = 1;
+
+            if (octaves < 0)
+                octaves = 0;
         }
     }
 }
