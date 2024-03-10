11e53525e09c48c2aaac08525b336f9d11eed807
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-03-11 00:09:49 +0300

    Updated Player Script
    In the Player script:
    - Added a field to reference a TileDictionarySO.
    - Added a field to reference a GameObjectRuntimeSet for the ground tilemap.
    - In the Start method:
      - Retrieved the Tilemap component from the GameObject in the runtime set.
      - Set the player's coordinates based on the Tilemap's cell position under the player.
      - Called a method to set the player's Tile Dictionary Index based on the ground tile under the player.
    - Added a method to set the player's Tile Dictionary Index based on the ground tile under the player.

--- Assets/_Script/Actors/Player.cs
+++ Assets/_Script/Actors/Player.cs
@@ -1,15 +1,31 @@
+using _Script.PersonalAPI.Data.RuntimeSet;
+using _Script.Tile;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 namespace _Script.Actors
 {
     public class Player : MonoBehaviour
     {
         [SerializeField] private PlayerDataSO _so_playerData;
-
-        private void Awake()
+        [SerializeField] private TileDictionarySO _so_tileDictionary;
+        [SerializeField] private GameObjectRuntimeSet _so_rs_tilemap_ground;
+        private Tilemap _tilemap_ground;
+        
+        private void Start()
         {
+            _tilemap_ground = _so_rs_tilemap_ground.Items[0].GetComponent<Tilemap>();
             Vector3 position = transform.position;
-            _so_playerData.PlayerCoord = new Vector3Int((int)position.x, (int)position.y, (int)position.z);
+            _so_playerData.PlayerCoord = _tilemap_ground.WorldToCell(position);
+            SetPlayerTileDictIndex();
         }
+        
+        private void SetPlayerTileDictIndex()
+        {
+            GroundTileData tileUnderPlayer = _so_tileDictionary.GetTileData(_so_playerData.PlayerCoord);
+            _so_playerData.TileUnderThePlayer = tileUnderPlayer;
+            _so_playerData.PlayerTileDictIndex = tileUnderPlayer.DictIndex;
+        }
+
     }
 }
