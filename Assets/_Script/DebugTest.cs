01b6b8001cb09a665d35bbeaa9d96bf4024ddc72
erkam_uzuncayir <erkamuzuncayir@gmail.com>
2024-03-24 05:01:15 +0300

    continue test

--- Assets/_Script/DebugTest.cs
+++ Assets/_Script/DebugTest.cs
@@ -9,7 +9,18 @@ public class DebugTest : MonoBehaviour
         Vector3 startPos = new Vector3(0, 0, 0);
         Vector3 destPos = new Vector3(-10, 5, 0);
         float maxStep = 3f;
-        Vector2 diff = new(Mathf.Abs(startPos.x - destPos.x), Mathf.Abs(startPos.y - destPos.y));
+        Vector2 diff = new Vector2(0, 0);
+        if (startPos.x > destPos.x)
+            diff.x = startPos.x - destPos.x;
+        else
+            diff.x = destPos.x - startPos.x;
+        
+        if (startPos.y > destPos.y)
+            diff.y = startPos.y - destPos.y;
+        else
+            diff.y = destPos.y - startPos.y;
+
+            
         // TODO: This should solve the problem
         Debug.Log(maxStep / (diff.x + diff.y) * diff);
         Debug.Log(Vector3.Lerp(startPos, destPos, maxStep / Vector3.Distance(startPos, destPos)));
